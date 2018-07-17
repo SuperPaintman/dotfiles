b08456a4439977335f3b498224aea2ad20acef72
SuperPaintman <SuperPaintmanDeveloper@gmail.com>
2018-07-17 23:04:27 +0300

    Updated `ZSH`

--- zsh/.zsh/oh-my-zsh-config.zsh
+++ zsh/.zsh/oh-my-zsh-config.zsh
@@ -1,4 +1,7 @@
 #!/usr/bin/env zsh
+# Init
+ZSH="$HOME/.oh-my-zsh"
+
 # Theme
 ZSH_THEME="amuse"
 
@@ -14,7 +17,7 @@ plugins=()
 ## Vendor
 #plugins+=(git)
 plugins+=(emoji)
-plugins+=(thefuck)
+#plugins+=(thefuck)
 #plugins+=(vi-mode)
 #plugins+=(web-search)
 plugins+=(colorize)
@@ -24,4 +27,4 @@ plugins+=(zsh-autosuggestions)
 plugins+=(zsh-syntax-highlighting)
 
 # Init
-source $ZSH/oh-my-zsh.sh
+source "$ZSH/oh-my-zsh.sh"
--- zsh/.zsh/theme.zsh
+++ zsh/.zsh/theme.zsh
@@ -2,12 +2,12 @@
 # Prompt
 ## Common
 _user_color="green"; [ $UID -eq 0 ] && _user_color="red"
-_user_emoji="$emoji[sheep]"; [ $UID -eq 0 ] && _user_emoji="$emoji[skull]"
 _user_caret=">"; [ $UID -eq 0 ] && _user_caret="#"
-
-#### Cygwin not support emojis
 if [ "$TERM" = cygwin ]; then
-    _user_emoji=""
+    # Cygwin is not supported emojis
+    _user_emoji="@"; [ $UID -eq 0 ] && _user_emoji="!"
+else
+    _user_emoji="$emoji[panda_face]"; [ $UID -eq 0 ] && _user_emoji="$emoji[skull]"
 fi
 
 _fish_pwd() {
@@ -18,17 +18,6 @@ _fish_pwd() {
     }; s|^$ENV{HOME}|~|g; s|/([^/.])[^/]*(?=/)|/$1|g; s|/\.([^/])[^/]*(?=/)|/.$1|g
     ')
 }
-
-_git_prompt_info_short() {
-    local ref
-
-    if [[ "$(command git config --get oh-my-zsh.hide-status 2>/dev/null)" != "1" ]]; then
-        ref=$(command git symbolic-ref HEAD 2> /dev/null) || \
-        ref=$(command git rev-parse --short HEAD 2> /dev/null) || return 0
-        echo "%{$fg[magenta]%}${ref#refs/heads/}$(parse_git_dirty)%{$reset_color%}"
-    fi
-}
-
 
 ## Left
 _prompt_left_long() {
@@ -69,37 +58,3 @@ _prompt_left() {
     fi
 }
 PROMPT='$(_prompt_left)'
-
-
-## Right
-_prompt_right_long() {
-    local ret_status="%{$fg_bold[red]%}%(?..%?)%{$reset_color%}"
-    local git_i="$(git_prompt_info)"
-    local git_s="$(git_prompt_status)"
-
-    echo "$ret_status$git_i$git_s%{$reset_color%}"
-}
-_prompt_right_medium() {
-    local ret_status="%{$fg_bold[red]%}%(?..%?)%{$reset_color%}"
-    local git_i="$(_git_prompt_info_short)"
-    local git_s="$(git_prompt_status)"
-
-    echo "$ret_status $git_i$git_s%{$reset_color%}"
-}
-_prompt_right_short() {
-    local ret_status="%{$fg_bold[red]%}%(?..%?)%{$reset_color%}"
-    local git_d="$(parse_git_dirty)"
-    local git_s="$(git_prompt_status)"
-
-    echo "$ret_status $git_d$git_s%{$reset_color%}"
-}
-_prompt_right() {
-    if [ "$COLUMNS" -ge 50 ]; then
-        _prompt_right_long
-    elif [ "$COLUMNS" -ge 40 ]; then
-        _prompt_right_medium
-    else
-        _prompt_right_short
-    fi
-}
-RPROMPT='$(_prompt_right)'
