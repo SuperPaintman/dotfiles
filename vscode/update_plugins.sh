e411731808aa20d3c99e9b08e92aef362957c30f
SuperPaintman <SuperPaintmanDeveloper@gmail.com>
2019-05-11 20:53:59 +0300

    Fix `update_plugins.sh` script

--- vscode/update_plugins.sh
+++ vscode/update_plugins.sh
@@ -2,4 +2,24 @@
 
 EXTENSIONS_FILE="$(dirname "$(realpath "${BASH_SOURCE[0]}")")/list-extensions.txt"
 
-code --list-extensions --show-versions | sort > "$EXTENSIONS_FILE"
+# Because every OS implements it's own sort function
+sort_words() {
+    node -e "$(cat <<EOF
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+
+var res = '';
+process.stdin
+    .on('data', function (chunk) {
+        res += chunk.toString();
+    })
+    .on('end', function () {
+        var result = res.split('\n').sort().join('\n').trim() + '\n';
+
+        process.stdout.write(result);
+    });
+EOF
+)"
+}
+
+code --list-extensions --show-versions | sort_words > "$EXTENSIONS_FILE"
