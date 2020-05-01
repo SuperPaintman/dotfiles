27f273d417aa284def62102630a141b69d8625b3
SuperPaintman <SuperPaintmanDeveloper@gmail.com>
2020-05-01 21:05:38 +0300

    feat(awesome): add link to the API doc

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -1,3 +1,5 @@
+-- See: https://awesomewm.org/doc/api/index.html.
+
 -- If LuaRocks is installed, make sure that packages installed through it are
 -- found (e.g. lgi). If LuaRocks is not installed, do nothing.
 pcall(require, "luarocks.loader")
