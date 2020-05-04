6eeabd49dad366d2fea3cd9385da32f26ec070bc
SuperPaintman <SuperPaintmanDeveloper@gmail.com>
2020-05-04 21:41:05 +0300

    feat(zsh): disable vim mode

--- zsh/.zsh/key-bindings.zsh
+++ zsh/.zsh/key-bindings.zsh
@@ -3,10 +3,13 @@
 # Note: ^ - Ctrl .
 
 # Use vim key bindings.
-bindkey -v
+# TDDO(SuperPaintman):
+#     One day... I wanna add mode indicator in the prompt.
+#     But I dont know how to detect VISUAL and REPLACE modes.
+# bindkey -v
 
 # Escape back to the normal mode.
-bindkey -M viins 'jk' vi-cmd-mode
+# bindkey -M viins 'jk' vi-cmd-mode
 
 # Accepts the current `zsh-autosuggestions` suggestion.
 bindkey -M viins '^ ' autosuggest-accept
