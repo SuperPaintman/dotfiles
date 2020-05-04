1a05d62240ada8365ce85c2eb6ba32668119e035
SuperPaintman <SuperPaintmanDeveloper@gmail.com>
2020-05-04 05:45:53 +0300

    refactor(bash): refactor aliases

--- bash/.bash/aliases.sh
+++ bash/.bash/aliases.sh
@@ -1,21 +1,68 @@
 #!/usr/bin/env bash
 
-# Common
-## `cat` aliases
+#===============================================================================
+# Replacements and default flags.
+#===============================================================================
+# `cat`.
 if can bat; then
-    alias cat="bat"
+    # Replace `cat` with `bat`.
+    alias cat='bat'
 fi
 
-## `cd` aliases
-alias ..="cd .."
-alias cd..="cd .."
-alias ...="cd ../.."
-alias ....="cd ../../.."
-alias .....="cd ../../../.."
-alias ......="cd ../../../../.."
-alias ~="cd ~"
+# `ls`.
+if can exa; then
+    # Replace `ls` with `exa`.
+    alias ls='exa --color=auto'
+elif can gls; then
+    alias ls='gls --color=auto'
+else
+    alias ls='ls --color=auto'
+fi
 
-if which lfcd > /dev/null 2>&1; then
+# `tree`.
+alias tree='tree -C'
+
+# `grep`.
+alias grep='grep --color=auto --exclude-dir={.bzr,CVS,.git,.hg,.svn}'
+
+# `fgrep`.
+alias fgrep='fgrep --color=auto'
+
+# `egrep`.
+alias egrep='egrep --color=auto'
+
+# `open`.
+if ! can open && can xdg-open; then
+    alias open='xdg-open'
+fi
+
+# `pbcopy`.
+if ! can pbcopy; then
+    alias pbcopy='xclip -selection clipboard'
+fi
+
+# `pbpaste`.
+if ! can pbpaste; then
+    alias pbpaste='xclip -selection clipboard -o'
+fi
+
+# `npx`
+alias npx='npx --no-install'
+
+#===============================================================================
+# Shortcats.
+#===============================================================================
+# `cd`.
+alias ..='cd ..'
+alias cd..='cd ..'
+alias ...='cd ../..'
+alias ....='cd ../../..'
+alias .....='cd ../../../..'
+alias ......='cd ../../../../..'
+alias ~='cd ~'
+
+# Use `lfcd` (`lf`) as `cd` without arguments.
+if can lfcd; then
     cd() {
         if [ "$#" = 0 ]; then
             lfcd
@@ -25,73 +72,43 @@ if which lfcd > /dev/null 2>&1; then
     }
 fi
 
-## `ls` aliases
-if can exa; then
-    alias ls="exa --color=auto"
-elif can gls; then
-    alias ls="gls --color=auto"
-else
-    alias ls="ls --color=auto"
-fi
-alias ll="ls -alFh --group-directories-first"
-alias la="ls -ah --group-directories-first"
-alias l="ls -Fh --group-directories-first"
+# `ls`.
+alias ll='ls -alFh --group-directories-first'
+alias la='ls -ah --group-directories-first'
+alias l='ls -Fh --group-directories-first'
 
-## `df` aliases
-alias dfh="df -h --total"
+# `df`.
+alias dfh='df -h --total'
 
-## `tree` aliases
-alias tree="tree -C"
-alias t="tree"
+# `tree`.
+alias t='tree'
 
-## `grep`, `fgrep`, `egrep` aliases
-alias grep="grep --color=auto --exclude-dir={.bzr,CVS,.git,.hg,.svn}"
-alias fgrep="fgrep --color=auto"
-alias egrep="egrep --color=auto"
+# `git`.
+alias g='git'
+alias gl='git l'
+alias glo='git lo'
+alias glol='git lol'
+alias gd='git d'
+alias gdi='git di'
+alias gs='git s'
+alias gcb='git cb'
+alias gcm='git cm'
 
-## Open
-if ! can open && can xdg-open; then
-    alias open="xdg-open"
-fi
+# `which`.
+alias where='which'
 
-## Other
+# `can`.
+alias has='can'
+
+# `python`.
+alias py='python'
+alias py3='python3'
+
+# `webpack-dev-server`.
+alias wds='npx webpack-dev-server'
+
+# Misc.
 alias e="$EDITOR"
-alias where="which"
-alias afk="systemctl suspend -i"
-alias fs='stat --format="%s bytes"'
 alias map="xargs -n1"
-
-# Git
-alias g="git"
-alias gl="git l"
-alias glo="git lo"
-alias glol="git lol"
-alias gd="git d"
-alias gdi="git di"
-alias gs="git s"
-alias gcb="git cb"
-alias gcm="git cm"
-
-# SSH
-alias sshpass="ssh -o PreferredAuthentications=password -o PubkeyAuthentication=no"
-
-# Python
-alias py="python"
-alias py3="python3"
-
-# Pygments
-alias ccat="colorize"
-
-# NPM and NPX
-alias npx="npx --no-install"
-
-# Webpack
-alias wds="npx webpack-dev-server"
-
-# `pbcopy` and `pbpaste`
-if ! can pbcopy; then
-    alias pbcopy="xclip -selection clipboard"
-fi
-if ! can pbpaste; then
-    alias pbpaste="xclip -selection clipboard -o"
-fi
+# alias afk="systemctl suspend -i"
+alias sshpass='ssh -o PreferredAuthentications=password -o PubkeyAuthentication=no'
