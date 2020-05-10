7563ccbc0f5480b260bce21ef045a496066cbfcb
SuperPaintman <SuperPaintmanDeveloper@gmail.com>
2020-05-10 19:08:34 +0300

    feat(awesome): change terminal and editor

--- awesome/autostart.sh
+++ awesome/autostart.sh
@@ -1,8 +1,3 @@
 #!/usr/bin/env bash
 
-# set -e
-
 # xrandr --output DVI-D-0 --left-of DVI-I-1
-
-
-exit 2
--- awesome/rc.lua
+++ awesome/rc.lua
@@ -63,8 +63,8 @@ end
 beautiful.init(gears.filesystem.get_themes_dir() .. "default/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
-terminal = "xterm"
-editor = os.getenv("EDITOR") or "nano"
+terminal = "alacritty"
+editor = os.getenv("EDITOR") or "vim"
 editor_cmd = terminal .. " -e " .. editor
 
 -- Default modkey.
