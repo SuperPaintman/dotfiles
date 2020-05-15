e10673e60f1c6c9fee85cbd84a2418b51da3606c
SuperPaintman <SuperPaintmanDeveloper@gmail.com>
2020-05-15 04:24:23 +0300

    feat(awesome): disable "awful.hotkeys_popup.keys"

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -18,7 +18,7 @@ local menubar = require("menubar")
 local hotkeys_popup = require("awful.hotkeys_popup")
 -- Enable hotkeys help widget for VIM and other apps
 -- when client with a matching name is opened:
-require("awful.hotkeys_popup.keys")
+-- require("awful.hotkeys_popup.keys")
 local dpi = require("beautiful.xresources").apply_dpi
 
 local keys = require("keys")
