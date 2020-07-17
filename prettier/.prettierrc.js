1b37d0df5f6c7eb8fe5bb168934b6cb5aa72368a
SuperPaintman <SuperPaintmanDeveloper@gmail.com>
2020-07-17 19:28:38 +0300

    feat(prettier): update config

--- prettier/.prettierrc.js
+++ prettier/.prettierrc.js
@@ -6,9 +6,12 @@
 module.exports = {
   printWidth: 80,
   tabWidth: 2,
+  useTabs: false,
   semi: true,
   singleQuote: true,
+  quoteProps: 'as-needed',
   jsxSingleQuote: true,
+  trailingComma: 'none',
   bracketSpacing: true,
   jsxBracketSameLine: true,
   arrowParens: 'always',
