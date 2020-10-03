079a84e23153498800e966d6f2be02d8a6632fe7
SuperPaintman <SuperPaintmanDeveloper@gmail.com>
2020-10-03 18:01:57 +0300

    chore(scripts): add files without extension into `format.sh` script

--- format.sh
+++ format.sh
@@ -1,21 +1,34 @@
 #!/usr/bin/env bash
 
 set -e
+set -u
+set -o pipefail
+
+# Helpers.
+filter_bash_content() {
+    while read filename; do
+        if grep '#!/usr/bin/env \(bash\|zsh\|sh\)' "$filename" > /dev/null; then
+            echo "$filename"
+        fi
+    done
+}
 
 # go get github.com/mvdan/sh/cmd/shfmt
-shfmt -i 4 -ci -sr -s -w $(
-    find . \
-        \( -type f -name '*.sh' -or -name '*.bash' -or -name '*.zsh' \) \
-        -and \
-        -not -path './zsh/.oh-my-zsh/**' \
-        -and \
-        -not -path './zsh/.oh-my-zsh-custom/**' \
-        -and \
-        -not -path './vim/.vim/bundle/**' \
-        -and \
-        -not -path './vim/.vim/plugged/**' \
-        -and \
-        -not -path './tmux/.tmux/plugins/**' \
-        -and \
-        -not -path '**/node_modules/**'
-)
+shfmt -i 4 -ci -sr -s -w \
+    $(
+        find . \
+            \( -type f -name '*.sh' -or -name '*.bash' -or -name '*.zsh' \) \
+            -and -not -path './zsh/.oh-my-zsh/**' \
+            -and -not -path './zsh/.oh-my-zsh-custom/**' \
+            -and -not -path './vim/.vim/bundle/**' \
+            -and -not -path './vim/.vim/plugged/**' \
+            -and -not -path './tmux/.tmux/plugins/**' \
+            -and -not -path '**/node_modules/**'
+    ) \
+    $(
+        find ./bin/bin ./rofi/modes \
+            -type f \
+            -and -not -path './bin/bin/neofetch' \
+            -and -not -path './bin/bin/styles' |
+            filter_bash_content
+    )
