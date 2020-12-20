653829e415ba4ec8e45cf0cc15390d14471b29d4
SuperPaintman <SuperPaintmanDeveloper@gmail.com>
2020-12-20 17:28:14 +0300

    refactor(awesome): move buttons for layouting into the main file

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -260,17 +260,99 @@ local function set_top_bar(s)
     local separator = wibox.widget.textbox("<span foreground='#FFFFFF3F'>|</span>")
 
     ---- Tagline.
+    local taglist_buttons = gears.table.join(
+        awful.button(
+            {},
+            1,
+            function(t)
+                t:view_only()
+            end
+        ),
+        awful.button(
+            {modkey},
+            1,
+            function(t)
+                if client.focus then
+                    client.focus:move_to_tag(t)
+                end
+            end
+        ),
+        awful.button(
+            {},
+            3,
+            awful.tag.viewtoggle
+        ),
+        awful.button(
+            {modkey},
+            3,
+            function(t)
+                if client.focus then
+                    client.focus:toggle_tag(t)
+                end
+            end
+        ),
+        awful.button(
+            {},
+            4,
+            function(t)
+                awful.tag.viewnext(t.screen)
+            end
+        ),
+        awful.button(
+            {},
+            5,
+            function(t)
+                awful.tag.viewprev(t.screen)
+            end
+        )
+    )
+
     local taglist = awful.widget.taglist {
         screen = s,
         filter = awful.widget.taglist.filter.all,
-        buttons = keys.taglist_buttons
+        buttons = taglist_buttons
     }
 
     ---- Tasklist.
+    local tasklist_buttons = gears.table.join(
+        awful.button(
+            {},
+            1,
+            function(c)
+                if c == client.focus then
+                    c.minimized = true
+                else
+                    c:emit_signal("request::activate", "tasklist", {raise = true})
+                end
+            end
+        ),
+        awful.button(
+            {},
+            3,
+            function()
+                awful.menu.client_list({theme = {width = 250}})
+            end
+        ),
+        awful.button(
+            {},
+            4,
+            function()
+                awful.client.focus.byidx(1)
+            end
+        ),
+        awful.button(
+            {},
+            5,
+            function()
+                awful.client.focus.byidx(-1)
+            end
+        )
+    )
+
     local tasklist = awful.widget.tasklist {
         screen = s,
         filter = awful.widget.tasklist.filter.currenttags,
-        buttons = keys.tasklist_buttons
+        buttons = tasklist_buttons
     }
 
     ---- Telegram.
