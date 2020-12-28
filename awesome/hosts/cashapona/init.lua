70ce7d056c1d6a46c1f4dff0b89eaf9e19554069
SuperPaintman <SuperPaintmanDeveloper@gmail.com>
2020-12-29 00:19:57 +0300

    feat(awesome): remove xrandr calls

--- awesome/hosts/cashapona/init.lua
+++
@@ -1,4 +0,0 @@
-local awful = require("awful")
-
--- Setup displays.
-awful.spawn("xrandr --output eDP-1 --primary --pos 0x1080 --output DP-3 --pos 0x0", {})
--- awesome/hosts/sequoia/init.lua
+++
@@ -1,4 +0,0 @@
-local awful = require("awful")
-
--- Setup displays.
-awful.spawn("xrandr --output DVI-D-0 --left-of DVI-I-1", {})
