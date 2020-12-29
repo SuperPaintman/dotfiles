3a3596398db11c31d2310032d1b5cbf0d0f7bc7f
SuperPaintman <SuperPaintmanDeveloper@gmail.com>
2020-12-29 23:13:50 +0300

    feat(awesome): remove CPU and RAM daemons

--- awesome/daemons/init.lua
+++ awesome/daemons/init.lua
@@ -1,6 +1,4 @@
 local daemons = {
-  cpu = require("daemons.cpu"),
-  ram = require("daemons.ram"),
   wifi_status = require("daemons.wifi_status"),
   battery = require("daemons.battery"),
   monitroid = require("daemons.monitroid"),
