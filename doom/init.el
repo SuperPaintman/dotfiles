4444e24b3e7f88534328ac6e4e3e29197ecfad43
SuperPaintman <SuperPaintmanDeveloper@gmail.com>
2021-01-18 02:23:46 +0300

    feat(doom): enable `make` module

--- doom/init.el
+++ doom/init.el
@@ -96,7 +96,7 @@
        lookup              ; navigate your code and its documentation
        (lsp +eglot)
        (magit +forge)      ; a git porcelain for Emacs
-       ;;make              ; run make tasks from Emacs
+       make                ; run make tasks from Emacs
        ;;pass              ; password manager for nerds
        ;;pdf               ; pdf enhancements
        ;;prodigy           ; FIXME managing external services & code builders
