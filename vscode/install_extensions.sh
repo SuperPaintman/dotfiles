74cace33589d2cffc2aa83e707f948ffd9cba615
SuperPaintman <SuperPaintmanDeveloper@gmail.com>
2021-05-09 21:50:29 +0300

    chore(scripts): refactor `generate-vscode-extensions`

--- vscode/install_extensions.sh
+++ vscode/install_extensions.sh
@@ -27,7 +27,7 @@ has_extension() {
     return 0
 }
 
-get_extension() {
+download_extension() {
     local publisher="$1"
     local name="$2"
     local version="$3"
@@ -47,144 +47,98 @@ get_extension() {
     mv "${tmp_ext_dir}/extension" "$ext_dir"
 }
 
+extension() {
+    local publisher="$1"
+    local name="$2"
+    local version="$3"
+
+    if ! has_extension "${publisher}" "${name}"; then
+        download_extension "${publisher}" "${name}" "${version}"
+    fi
+}
+
 mkdir -p "$HOME/.vscode/extensions"
 
 # cpptools (ms-vscode)
-if ! has_extension "ms-vscode" "cpptools"; then
-    get_extension "ms-vscode" "cpptools" "0.26.3"
-fi
+extension "ms-vscode" "cpptools" "0.26.3"
 
 # clang-format (xaver)
-if ! has_extension "xaver" "clang-format"; then
-    get_extension "xaver" "clang-format" "1.9.0"
-fi
+extension "xaver" "clang-format" "1.9.0"
 
 # cmake (twxs)
-if ! has_extension "twxs" "cmake"; then
-    get_extension "twxs" "cmake" "0.0.17"
-fi
+extension "twxs" "cmake" "0.0.17"
 
 # dart-code (Dart-Code)
-if ! has_extension "Dart-Code" "dart-code"; then
-    get_extension "Dart-Code" "dart-code" "3.11.0"
-fi
+extension "Dart-Code" "dart-code" "3.11.0"
 
 # elm-ls-vscode (Elmtooling)
-if ! has_extension "Elmtooling" "elm-ls-vscode"; then
-    get_extension "Elmtooling" "elm-ls-vscode" "1.5.2"
-fi
+extension "Elmtooling" "elm-ls-vscode" "1.5.2"
 
 # flutter (Dart-Code)
-if ! has_extension "Dart-Code" "flutter"; then
-    get_extension "Dart-Code" "flutter" "3.12.2"
-fi
+extension "Dart-Code" "flutter" "3.12.2"
 
 # Go (golang)
-if ! has_extension "golang" "Go"; then
-    get_extension "golang" "Go" "0.14.1"
-fi
+extension "golang" "Go" "0.14.1"
 
 # language-haskell (justusadam)
-if ! has_extension "justusadam" "language-haskell"; then
-    get_extension "justusadam" "language-haskell" "3.3.0"
-fi
+extension "justusadam" "language-haskell" "3.3.0"
 
 # vscode-ghc-simple (dramforever)
-if ! has_extension "dramforever" "vscode-ghc-simple"; then
-    get_extension "dramforever" "vscode-ghc-simple" "0.1.22"
-fi
+extension "dramforever" "vscode-ghc-simple" "0.1.22"
 
 # brittany (MaxGabriel)
-if ! has_extension "MaxGabriel" "brittany"; then
-    get_extension "MaxGabriel" "brittany" "0.0.9"
-fi
+extension "MaxGabriel" "brittany" "0.0.9"
 
 # lua (sumneko)
-if ! has_extension "sumneko" "lua"; then
-    get_extension "sumneko" "lua" "0.17.0"
-fi
+extension "sumneko" "lua" "0.17.0"
 
 # Nix (bbenoist)
-if ! has_extension "bbenoist" "Nix"; then
-    get_extension "bbenoist" "Nix" "1.0.1"
-fi
+extension "bbenoist" "Nix" "1.0.1"
 
 # python (ms-python)
-if ! has_extension "ms-python" "python"; then
-    get_extension "ms-python" "python" "2020.9.114305"
-fi
+extension "ms-python" "python" "2020.9.114305"
 
 # rust-analyzer (matklad)
-if ! has_extension "matklad" "rust-analyzer"; then
-    get_extension "matklad" "rust-analyzer" "0.2.583"
-fi
+extension "matklad" "rust-analyzer" "0.2.583"
 
 # stylus (alan)
-if ! has_extension "alan" "stylus"; then
-    get_extension "alan" "stylus" "0.0.4"
-fi
+extension "alan" "stylus" "0.0.4"
 
 # vimL (fallenwood)
-if ! has_extension "fallenwood" "vimL"; then
-    get_extension "fallenwood" "vimL" "0.0.3"
-fi
+extension "fallenwood" "vimL" "0.0.3"
 
 # monokai-extended (SuperPaintman)
-if ! has_extension "SuperPaintman" "monokai-extended"; then
-    get_extension "SuperPaintman" "monokai-extended" "0.5.1"
-fi
+extension "SuperPaintman" "monokai-extended" "0.5.1"
 
 # even-better-toml (tamasfe)
-if ! has_extension "tamasfe" "even-better-toml"; then
-    get_extension "tamasfe" "even-better-toml" "0.12.1"
-fi
+extension "tamasfe" "even-better-toml" "0.12.1"
 
 # terraform (HashiCorp)
-if ! has_extension "HashiCorp" "terraform"; then
-    get_extension "HashiCorp" "terraform" "2.10.2"
-fi
+extension "HashiCorp" "terraform" "2.10.2"
 
 # docthis (oouo-diogo-perdigao)
-if ! has_extension "oouo-diogo-perdigao" "docthis"; then
-    get_extension "oouo-diogo-perdigao" "docthis" "0.8.2"
-fi
+extension "oouo-diogo-perdigao" "docthis" "0.8.2"
 
 # llvm (RReverser)
-if ! has_extension "RReverser" "llvm"; then
-    get_extension "RReverser" "llvm" "0.0.3"
-fi
+extension "RReverser" "llvm" "0.0.3"
 
 # prettier-vscode (esbenp)
-if ! has_extension "esbenp" "prettier-vscode"; then
-    get_extension "esbenp" "prettier-vscode" "4.5.0"
-fi
+extension "esbenp" "prettier-vscode" "4.5.0"
 
 # quokka-vscode (WallabyJs)
-if ! has_extension "WallabyJs" "quokka-vscode"; then
-    get_extension "WallabyJs" "quokka-vscode" "1.0.291"
-fi
+extension "WallabyJs" "quokka-vscode" "1.0.291"
 
 # vscode-arduino (vsciot-vscode)
-if ! has_extension "vsciot-vscode" "vscode-arduino"; then
-    get_extension "vsciot-vscode" "vscode-arduino" "0.2.29"
-fi
+extension "vsciot-vscode" "vscode-arduino" "0.2.29"
 
 # gitignore (codezombiech)
-if ! has_extension "codezombiech" "gitignore"; then
-    get_extension "codezombiech" "gitignore" "0.6.0"
-fi
+extension "codezombiech" "gitignore" "0.6.0"
 
 # rest-client (humao)
-if ! has_extension "humao" "rest-client"; then
-    get_extension "humao" "rest-client" "0.24.3"
-fi
+extension "humao" "rest-client" "0.24.3"
 
 # partial-diff (ryu1kn)
-if ! has_extension "ryu1kn" "partial-diff"; then
-    get_extension "ryu1kn" "partial-diff" "1.4.1"
-fi
+extension "ryu1kn" "partial-diff" "1.4.1"
 
 # jinjahtml (samuelcolvin)
-if ! has_extension "samuelcolvin" "jinjahtml"; then
-    get_extension "samuelcolvin" "jinjahtml" "0.15.0"
-fi
+extension "samuelcolvin" "jinjahtml" "0.15.0"
