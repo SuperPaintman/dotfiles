9114b17f6456bc42162238a046c4eb771673fe7c
SuperPaintman <SuperPaintmanDeveloper@gmail.com>
2021-09-27 02:05:07 +0300

    feat(zsh): add completions for Go

--- zsh/.zsh/completions.zsh
+++ zsh/.zsh/completions.zsh
@@ -1,5 +1,12 @@
 #!/usr/bin/env zsh
 
+# Recompile completions.
+#
+#   $ rm ~/.zcompdump; compinit
+
+# Add custom completions path.
+fpath+=(~/.zsh/completions)
+
 if can fzf-share && [ -f "$(fzf-share)/completion.zsh" ]; then
     source "$(fzf-share)/completion.zsh"
 elif can brew && brew --prefix fzf > /dev/null 2>&1 && [ -f "$(brew --prefix fzf)/shell/completion.zsh" ]; then
