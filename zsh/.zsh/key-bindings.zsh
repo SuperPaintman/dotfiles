1e931f4e8fed158447ae0e65f45bc2c91eeae322
SuperPaintman <SuperPaintmanDeveloper@gmail.com>
2021-11-03 11:12:33 +0300

    feat(zsh): add a key binding (<Esc>e) for editing current buffer

--- zsh/.zsh/key-bindings.zsh
+++ zsh/.zsh/key-bindings.zsh
@@ -55,5 +55,37 @@ if can project-find; then
     bindkey '\ep' project-find-cd-widget
 fi
 
+## Edit buffer in $EDITOR.
+edit-zsh-buffer() {
+    local temp_file="/tmp/edit-zsh-buffer.$$.txt"
+    rm -f "$temp_file"
+    touch "$temp_file"
+    echo "$LBUFFER" > "$temp_file"
+
+    local editor="$EDITOR"
+    if [ -z "$editor" ]; then
+        editor="vim"
+    fi
+
+    local options=()
+    case "$editor" in
+        nvim | vim)
+            # Auto enter into INSERT mode.
+            options=('-c' 'call feedkeys("\<Esc>GA")')
+            ;;
+    esac
+
+    $editor $options "$temp_file" < /dev/tty > /dev/tty || {
+        local ret="$?"
+        rm -f "$temp_file"
+        return "$ret"
+    }
+
+    LBUFFER="$(cat "$temp_file")"
+    rm -r "$temp_file"
+}
+zle -N edit-zsh-buffer
+bindkey '\ee' edit-zsh-buffer
+
 # Unbindings.
 bindkey -r '^s' # history-incremental-search-forward.
