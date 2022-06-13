a08036a5b7ae5332c125bed344c824dc235d0485
SuperPaintman <SuperPaintmanDeveloper@gmail.com>
2022-06-13 20:15:36 +0300

    feat(nixos): add more functions into the `lib.x`

--- nixos/lib/default.nix
+++ nixos/lib/default.nix
@@ -1,7 +1,240 @@
-{ lib }:
+{ pkgs, lib }:
 let
-  x = self: {
+  fromJSON = builtins.fromJSON;
+
+  toJSON = builtins.toJSON;
+
+  executeCommand' = name: command: pkgs.stdenv.mkDerivation {
+    inherit name;
+
+    phases = [ "buildPhase" ];
+
+    src = pkgs.writeText "${name}-command" command;
+
+    buildPhase = ''
+      mkdir -p $out
+
+      code=0
+      ${pkgs.runtimeShell} $src 1>$out/stdout 2>$out/stderr || { code="$?"; }
+
+      echo $code > $out/code
+    '';
+  };
+
+  executeCommand = name: command:
+    let
+      res = executeCommand' name command;
+    in
+    {
+      stdout = builtins.readFile "${res}/stdout";
+      stderr = builtins.readFile "${res}/stderr";
+      code = lib.toInt (builtins.readFile "${res}/code");
+    };
+
+  execute = name: command:
+    let
+      res = executeCommand name command;
+    in
+    assert lib.assertMsg (res.code == 0) res.stderr;
+    res.stdout;
+
+  executeJS = name: script:
+    let
+      scriptFile = pkgs.writeText "${name}.js" script;
+    in
+    execute name ''
+      ${pkgs.nodejs}/bin/node ${scriptFile}
+    '';
+
+  jsYAML = pkgs.fetchgit {
+    url = "https://github.com/nodeca/js-yaml.git";
+    rev = "4.1.0";
+    sha256 = "0h7f8p2xqfgy65x8lwjsy1riba1wvq7292wds1l8izk8kwivmdmf";
+  };
+
+  tinycolorJS = pkgs.fetchgit {
+    url = "https://github.com/bgrins/TinyColor.git";
+    rev = "1.4.2";
+    sha256 = "17hnqw922dlfmn5dl7b6dki9hhjlg9z7272g93dayb9yg7i48s43";
+  };
+
+  tinycolor = script: fromJSON (executeJS "tinycolor" ''
+    const tinycolor = require(${toJSON "${tinycolorJS}/dist/tinycolor-min.js"});
+
+    const script = (
+      ${script}
+    );
+
+    const res = script(tinycolor)
+
+    process.stdout.write(JSON.stringify(res));
+  '');
+
+  # charToHexMap = {
+  #   "0" = 0;
+  #   "1" = 1;
+  #   "2" = 2;
+  #   "3" = 3;
+  #   "4" = 4;
+  #   "5" = 5;
+  #   "6" = 6;
+  #   "7" = 7;
+  #   "8" = 8;
+  #   "9" = 9;
+  #   "a" = 10;
+  #   "A" = 10;
+  #   "b" = 11;
+  #   "B" = 11;
+  #   "c" = 12;
+  #   "C" = 12;
+  #   "d" = 13;
+  #   "D" = 13;
+  #   "e" = 14;
+  #   "E" = 14;
+  #   "f" = 15;
+  #   "F" = 15;
+  # };
+
+  # hexToCharMap = {
+  #   "0" = "0";
+  #   "1" = "1";
+  #   "2" = "2";
+  #   "3" = "3";
+  #   "4" = "4";
+  #   "5" = "5";
+  #   "6" = "6";
+  #   "7" = "7";
+  #   "8" = "8";
+  #   "9" = "9";
+  #   "10" = "A";
+  #   "11" = "B";
+  #   "12" = "C";
+  #   "13" = "D";
+  #   "14" = "E";
+  #   "15" = "F";
+  # };
+
+  # parseHexademicalChar = c:
+  #   if builtins.hasAttr c charToHexMap
+  #   then charToHexMap."${c}"
+  #   else null;
+
+  # parseHexadecimal = value:
+  #   let
+  #     len = builtins.stringLength value;
+  #   in
+  #   if len == 0
+  #   then 0
+  #   else
+  #     let
+  #       v = parseHexademicalChar (builtins.substring (len - 1) 1 value);
+  #     in
+  #     assert v != null;
+  #     v + (parseHexadecimal (builtins.substring 0 (len - 1) value)) * 16;
+
+  # hexadecimalToString = value:
+  #   let
+  #     head = lib.mod value 16;
+  #     tail = builtins.div value 16;
+  #     h = v:
+  #       assert builtins.hasAttr (builtins.toString v) hexToCharMap;
+  #       hexToCharMap."${builtins.toString v}";
+  #   in
+  #   if tail == 0
+  #   then (h head)
+  #   else (hexadecimalToString tail) + (h head);
+
+  # assertRGBA = { r, g, b, a ? 255 }:
+  #   assert r >= 0 && r <= 255;
+  #   assert b >= 0 && b <= 255;
+  #   assert g >= 0 && g <= 255;
+  #   assert a == null || a >= 0 && a <= 255;
+  #   true;
+
+  x = self: rec {
+    inherit executeCommand execute executeJS;
+
     callIfFunction = f: args: if builtins.isFunction f then (f args) else f;
+
+    fromYAML = content: fromJSON (executeJS "from-yaml" ''
+      const fs = require("fs");
+      const YAML = require(${toJSON "${jsYAML}/dist/js-yaml.min.js"});
+
+      const content = fs.readFileSync(
+        ${toJSON (pkgs.writeText "from-yaml-content" content)},
+        "utf-8"
+      );
+
+      process.stdout.write(JSON.stringify(YAML.load(content)));
+    '');
+
+    toYAML = v: executeJS "from-yaml" ''
+      const YAML = require(${toJSON "${jsYAML}/dist/js-yaml.min.js"});
+
+      process.stdout.write(YAML.dump(${toJSON v}));
+    '';
+
+    colors = rec {
+      # toRGBA' = parseAlpha: color:
+      #   if builtins.isAttrs color
+      #   then
+      #     assert assertRGBA color;
+      #     color
+      #   else
+      #     let
+      #       len = builtins.stringLength color;
+      #     in
+      #     assert (builtins.substring 0 1 color) == "#";
+      #     assert len == 7 || len == 9;
+      #     let
+      #       c = n: parseHexadecimal (builtins.substring (1 + 2 * n) 2 color);
+      #       r = c 0;
+      #       g = c 1;
+      #       b = c 2;
+      #       a =
+      #         if parseAlpha && len == 9
+      #         then c 3
+      #         else 255;
+      #     in
+      #     assert assertRGBA { inherit r g b a; };
+      #     {
+      #       inherit r g b;
+      #     } // (if parseAlpha then { inherit a; } else { });
+
+      # toRGB = toRGBA' false;
+
+      # toRGBA = toRGBA' true;
+
+      # toHEX = { r, g, b, a ? null }:
+      #   assert assertRGBA { inherit r g b a; };
+      #   let
+      #     h = hexadecimalToString;
+      #   in
+      #   "#${h r}${h g}${h b}${if a == null then "" else h a}";
+
+      toRGB = color: tinycolor ''
+        (v) => {
+          const res = v(${toJSON color}).toRgb();
+          delete res.a;
+          return res;
+        }
+      '';
+
+      toRGBA = color: tinycolor "(v) => v(${toJSON color}).toRgb()";
+
+      modify = name: amount: color: tinycolor ''
+        (v) => v(${toJSON color}).${name}(${toJSON amount})
+          .toHex8String()
+          .replace(/ff$/i, "")
+          .toUpperCase()
+      '';
+
+      lighten = modify "lighten";
+
+      brighten = modify "brighten";
+
+      darken = modify "darken";
+    };
   };
 in
 lib.makeExtensible x
--- nixos/lib/default.test.nix
+++ nixos/lib/default.test.nix
@@ -14,4 +14,122 @@ runTests {
     expr = x.callIfFunction 7331 1337;
     expected = 7331;
   };
+
+  testExecuteCommandWithOKScript = {
+    expr = x.executeCommand "test" ''
+      echo Hello
+    '';
+    expected = { stdout = "Hello\n"; stderr = ""; code = 0; };
+  };
+
+  testExecuteCommandWithBadScript = {
+    expr = x.executeCommand "test" ''
+      echo Bad >&2
+      exit 23
+    '';
+    expected = { stdout = ""; stderr = "Bad\n"; code = 23; };
+  };
+
+  testExecuteWithOKScript = {
+    expr = x.execute "test" ''
+      echo Hello
+    '';
+    expected = "Hello\n";
+  };
+
+  testExecuteJSWithOKScript = {
+    expr = x.executeJS "test" ''
+      console.log("Hello, JS");
+    '';
+    expected = "Hello, JS\n";
+  };
+
+  testFromYAML = {
+    expr = x.fromYAML ''
+      - Hello
+      - 42
+      - far:
+          away: true
+    '';
+    expected = [ "Hello" 42 { far.away = true; } ];
+  };
+
+  testToYAML = {
+    expr = x.toYAML {
+      hello = "world";
+      answer = 42;
+      far.away = true;
+    };
+    expected = ''
+      answer: 42
+      far:
+        away: true
+      hello: world
+    '';
+  };
+
+  # testToRGBWithValidRGBHexColor = {
+  #   expr = x.colors.toRGB "#ABCDEF";
+  #   expected = { r = 171; g = 205; b = 239; };
+  # };
+
+  # testToRGBWithValidRGBAHexColor = {
+  #   expr = x.colors.toRGB "#ABCDEF12";
+  #   expected = { r = 171; g = 205; b = 239; };
+  # };
+
+  # testToRGBAWithValidRGBHexColor = {
+  #   expr = x.colors.toRGBA "#ABCDEF";
+  #   expected = { r = 171; g = 205; b = 239; a = 255; };
+  # };
+
+  # testToRGBAWithValidRGBAHexColor = {
+  #   expr = x.colors.toRGBA "#ABCDEF12";
+  #   expected = { r = 171; g = 205; b = 239; a = 18; };
+  # };
+
+  # testToRGBAWithValidRGBColor = {
+  #   expr = x.colors.toRGBA { r = 171; g = 205; b = 239; a = 255; };
+  #   expected = { r = 171; g = 205; b = 239; a = 255; };
+  # };
+
+  # testToRGBAWithValidRGBAColor = {
+  #   expr = x.colors.toRGBA { r = 171; g = 205; b = 239; a = 18; };
+  #   expected = { r = 171; g = 205; b = 239; a = 18; };
+  # };
+
+  # testToHEXWithValieRGBColor = {
+  #   expr = x.colors.toHEX { r = 171; g = 205; b = 239; };
+  #   expected = "#ABCDEF";
+  # };
+
+  # testToHEXWithValieRGBAColor = {
+  #   expr = x.colors.toHEX { r = 171; g = 205; b = 239; a = 18; };
+  #   expected = "#ABCDEF12";
+  # };
+
+  testColorsToRGB = {
+    expr = x.colors.toRGB "#ABCDEF";
+    expected = { r = 171; g = 205; b = 239; };
+  };
+
+  testColorsToRGBA = {
+    expr = x.colors.toRGBA "#ABCDEF";
+    expected = { r = 171; g = 205; b = 239; a = 1; };
+  };
+
+  testColorsLighten = {
+    expr = x.colors.lighten 10 "#f00";
+    expected = "#FF3333";
+  };
+
+  testColorsBrighten = {
+    expr = x.colors.brighten 10 "#f00";
+    expected = "#FF1919";
+  };
+
+  testColorsDarken = {
+    expr = x.colors.darken 10 "#f00";
+    expected = "#CC0000";
+  };
 }
